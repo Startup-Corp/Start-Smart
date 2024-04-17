d7929e322c721278f0ae6e18e350e6d2e989b75f
yourd8ne <yourd8ne@mail.ru>
2024-04-17 18:07:02 +0300

    create default responses and test it

---
+++ back/app.py
@@ -0,0 +1,55 @@
+from flask import Flask, render_template, request, jsonify
+import supabase
+from supabase import create_client, Client
+import configparser 
+
+app = Flask(__name__)
+
+config = configparser.ConfigParser()
+config.read('./config.ini')
+
+supabaseURL = config['supabase']['supabaseURL']
+supabaseKey = config['supabase']['supabaseKey']
+
+
+supabase: Client = create_client(supabaseURL, supabaseKey)
+
+
+def main():
+    pass
+    
+@app.route('/get_users', methods=['GET'])
+def get_users():
+    response = supabase.table('Users').select('*').execute()
+    # Проверка наличия ошибок
+    return jsonify(response.data), 200
+    
+
+@app.route('/get_projects', methods=['GET'])
+def get_projects():
+    response = supabase.table('Projects').select('*').execute()
+    return jsonify(response.data), 200
+
+@app.route('/new_user', methods=['POST'])
+def create_new_user():
+    data = request.json
+    
+    rate = data.get('rate')
+    name = data.get('name')
+    email = data.get('email')
+
+    response = supabase.table('Users').insert([{'rate': rate, 'name': name, 'email': email}]).execute()
+
+    return jsonify({'message': 'Data saved successfully'}), 200
+
+@app.route('/del_user', methods=['POST'])
+def delete_user():
+    data = request.json
+    
+    id = data.get('id')
+
+    response = supabase.table('Users').delete().eq('id', id).execute()
+    return jsonify({'message': 'Data delete successfully'}), 200
+
+if __name__ == '__main__':
+    app.run()
---
+++ back/test_api.py
@@ -0,0 +1,33 @@
+import requests
+
+base_url = 'http://127.0.0.1:5000'
+
+
+def test_get_users():
+    response = requests.get(f'{base_url}/all_users')
+    assert response.status_code == 200
+
+
+def test_get_projects():
+    response = requests.get(f'{base_url}/get_projects')
+    assert response.status_code == 200
+
+
+def test_create_new_user():
+    data = {'rate': 0, 'name': 'adfh', 'email': 'testsfgjsfgj@example.com'}
+    response = requests.post(f'{base_url}/new_user', json=data)
+    assert response.status_code == 200
+    assert response.json()['message'] == 'Data saved successfully'
+
+
+def test_delete_user():
+    data = {'id': 7}  
+    response = requests.post(f'{base_url}/del_user', json=data)
+    assert response.status_code == 200
+    assert response.json()['message'] == 'Data delete successfully'
+
+if __name__ == '__main__':
+    # test_get_users()
+    # test_get_projects()
+    #test_create_new_user()
+    test_delete_user()
