e534d8307a06a9783109dceb7c74510d094562c0
Zhenya_Kulaev <kylaev11@gmail.com>
2024-06-28 13:20:18 +0300

    style fix

--- static/js/checkUsers.js
+++ static/js/checkUsers.js
@@ -1,33 +1,31 @@
-const btnReg = document.getElementById('btn-authorization');
+const btnReg = document.getElementById("btn-authorization");
 
-btnReg.addEventListener('click', async ()=> {
-    const mail = document.getElementById('mail').value;
-    const password = document.getElementById('password').value;
-    
-    const data = {
-        mailUser: mail,
-        passwordUser: password,
-    };
+btnReg.addEventListener("click", async () => {
+  const mail = document.getElementById("mail").value;
+  const password = document.getElementById("password").value;
 
-    try {
-        
-        const response = await fetch('/get_user', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(data)
-        });
-    
-        if (response.ok) {
-            alert('Добро пожаловать!');
+  const data = {
+    mailUser: mail,
+    passwordUser: password,
+  };
 
-            window.location.href = "http://127.0.0.1:5000/my_projects"
+  try {
+    const response = await fetch("/get_user", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
 
-        } else if (response.status === 404) {
-            alert('Пользователь не найден');
-        }
-    }  catch (error) {
-        alert(`Request failed: ${error.message}`);
+    if (response.ok) {
+      alert("Добро пожаловать!");
+
+      window.location.href = "http://127.0.0.1:5000/my_projects";
+    } else if (response.status === 404) {
+      alert("Пользователь не найден");
     }
+  } catch (error) {
+    alert(`Request failed: ${error.message}`);
+  }
 });
