369b1e581503f95308be253f9e8d416f7d75922c
Zhenya_Kulaev <kylaev11@gmail.com>
2024-07-30 22:27:27 +0300

    add data for project

--- back/app.py
+++ back/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify, render_template, session
 from objects.dbManager import DB_manager
+from gotrue import errors
 from routes.auth import auth_api, login_is_required
 import configparser
 import logging
@@ -24,11 +25,12 @@ def main():
 
 @app.route('/set_nickname', methods = ['POST'])
 def set_nickname():
-    data = request.json
+    data_id = request.get_json()
+    # print(data)
     
-    id = data.get('id')
+    # id = data.get('id')
     
-    user_nickname_response = db.select(table="Users", columns="name" ,criteria={"id": id})
+    user_nickname_response = db.select(table="Users", columns="name" ,criteria={"id": data_id})
     
     nickname = user_nickname_response.data[0].get('name') if user_nickname_response.data else None
 
--- static/js/authorization.js
+++ static/js/authorization.js
@@ -1,3 +1,25 @@
+// async function getNickaname(userId) {
+//   const idData = {
+//     id: userId,
+//   };
+
+//   fetch("/set_nickname", {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify(idData),
+//   })
+//     .then((response) => response.json())
+//     .then((data) => {
+//       console.log("Success:", data);
+//     })
+//     .catch((error) => {
+//       console.error("Error:", error);
+//     });
+// }
+
+
 const btnReg = document.getElementById("btn-authorization");
 
 btnReg.addEventListener("click", async () => {
@@ -6,30 +28,35 @@ btnReg.addEventListener("click", async () => {
 
   if (email.length === 0 || password.length === 0) {
     alert("Заполните все поля!");
-  } else {
-    const data = {
-      email: email,
-      passwordUser: password,
-    };
-
-    try {
-      const response = await fetch("/auth", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(data),
-      });
-
-      if (response.ok) {
-        showSuccessAlert();
-      } else if (response.status === 404) {
-        showErrorAlert();
-      }
-    } catch (error) {
-      alert(`Request failed: ${error.message}`);
-    }
+    return;
   }
+  const data = {
+    email: email,
+    passwordUser: password,
+  };
+
+  try {
+    const response = await fetch("/auth", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+
+    if (response.ok) {
+      // const responseData = await response.json();
+      // const userId = responseData.data.user_id;
+
+      // await getNickaname(userId);
+      showSuccessAlert();
+    } else if (response.status === 404) {
+      showErrorAlert();
+    }
+  } catch (error) {
+    alert(`Request failed: ${error.message}`);
+  }
+
 });
 
 function showSuccessAlert() {
