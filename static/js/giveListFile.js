171ce8d4ddd0caf27eb0adb68314514fa022f94a
Zhenya_Kulaev <kylaev11@gmail.com>
2024-08-04 16:04:30 +0300

    fix auth/reg and files

--- static/js/giveListFile.js
+++ static/js/giveListFile.js
@@ -1,19 +1,38 @@
 const fileInput = document.getElementById("fileInput");
 const fileDisplay = document.querySelector(".file-input-div ul");
-const maxCountFile = 5;
+const maxCountFile = 3;
+let currentFileCount = 0;
 
 fileInput.addEventListener("change", (event) => {
   const files = event.target.files;
+  const validExtensions = ["image/png", "image/jpeg"];
 
-  if (files.length > maxCountFile) {
+  if (currentFileCount + files.length > maxCountFile) {
     alert(`Можно выбрать не более ${maxCountFile} файлов.`);
     fileInput.value = "";
     return;
   }
 
   for (const file of files) {
+    if (!validExtensions.includes(file.type)) {
+      alert("Можно прикреплять только файлы с расширениями .png и .jpg.");
+      fileInput.value = "";
+      return;
+    }
+  }
+
+  for (const file of files) {
     const listItem = document.createElement("li");
     listItem.textContent = file.name;
+
+    listItem.addEventListener("click", () => {
+      listItem.remove();
+      currentFileCount--;
+    });
+
     fileDisplay.appendChild(listItem);
   }
+
+  currentFileCount += files.length;
+  fileInput.value = "";
 });
