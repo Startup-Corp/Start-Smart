baa805fc5087eb7afb60b546f1c6754e3b491eca
Zhenya_Kulaev <kylaev11@gmail.com>
2024-08-06 22:36:18 +0300

    fix lookPassword

--- static/js/lookPassword.js
+++ static/js/lookPassword.js
@@ -1,27 +1,32 @@
-const togglePassword = document.getElementById('toggle-password');
-togglePassword.addEventListener('click', function() {
-    let passwordInput = document.getElementById('password');
-    let eyeIcon = document.getElementById('eye-icon');
+const togglePassword = document.getElementById("toggle-password");
+if (togglePassword) {
+  togglePassword.addEventListener("click", function () {
+    let passwordInput = document.getElementById("password");
+    let eyeIcon = document.getElementById("eye-icon");
 
-    if (passwordInput.type === 'password') {
-        passwordInput.type = 'text';
-        eyeIcon.src = '/static/css/Fonts/Icons_for_registration/icons8-eye-slash.svg'; // скрыть пароль
+    if (passwordInput.type === "password") {
+      passwordInput.type = "text";
+      eyeIcon.src =
+        "/static/css/Fonts/Icons_for_registration/icons8-eye-slash.svg"; // скрыть пароль
     } else {
-        passwordInput.type = 'password';
-        eyeIcon.src = '/static/css/Fonts/Icons_for_registration/icons8-eye.svg'; // показать пароли
+      passwordInput.type = "password";
+      eyeIcon.src = "/static/css/Fonts/Icons_for_registration/icons8-eye.svg"; // показать пароли
     }
-});
+  });
+}
+const toggleRepPassword = document.getElementById("toggle-repPassword");
+if (toggleRepPassword) {
+  toggleRepPassword.addEventListener("click", function () {
+    let passwordRepInput = document.getElementById("repPassword");
+    let eyeIcon = document.getElementById("eye-icon-rep");
 
-const toggleRepPassword = document.getElementById('toggle-repPassword');
-toggleRepPassword.addEventListener('click', function() {
-    let passwordRepInput = document.getElementById('repPassword');
-    let eyeIcon = document.getElementById('eye-icon-rep');
-
-    if (passwordRepInput.type === 'password') {
-        passwordRepInput.type = 'text';
-        eyeIcon.src = '/static/css/Fonts/Icons_for_registration/icons8-eye-slash.svg'; // скрыть пароль
+    if (passwordRepInput.type === "password") {
+      passwordRepInput.type = "text";
+      eyeIcon.src =
+        "/static/css/Fonts/Icons_for_registration/icons8-eye-slash.svg"; // скрыть пароль
     } else {
-        passwordRepInput.type = 'password';
-        eyeIcon.src = '/static/css/Fonts/Icons_for_registration/icons8-eye.svg'; // показать пароли
+      passwordRepInput.type = "password";
+      eyeIcon.src = "/static/css/Fonts/Icons_for_registration/icons8-eye.svg"; // показать пароли
     }
-});  
+  });
+}
