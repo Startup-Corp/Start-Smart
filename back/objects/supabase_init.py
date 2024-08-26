c2d9ed44ed8149dd9c80eefa273b9e694ae7332c
Никита Кораблев <kit06092003@gmail.com>
2024-08-26 15:26:09 +0300

    updated config.ini connection

--- back/objects/supabase_init.py
+++ back/objects/supabase_init.py
@@ -1,8 +1,12 @@
+import os
+
 from supabase import create_client, Client
 import configparser
 
+# print(os.getcwd())
+
 config = configparser.ConfigParser()
-config.read('../config.ini')
+config.read('./config.ini')
 
 url: str = config['supabase']['url']
 key: str = config['supabase']['key']
